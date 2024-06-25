30e10fe075e8cc2af5f3ea29eb5fa6e7367f0c44
Anurag41682 <anuragyadav41682@gmail.com>
2024-06-25 19:37:10 +0530

    added mediaQuery and custom.d.ts

--- src/section/navbar/Navbar.tsx
+++ src/section/navbar/Navbar.tsx
@@ -1,8 +1,15 @@
+import Logo from "../../assets/logo.jpg";
 import "./navbar.css";
 const Navbar = () => {
   return (
     <nav>
-      <h2>Navbar</h2>
+      <div className="container nav_container">
+        <a>
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className="nav_menu"></ul>
+        <button></button>
+      </div>
     </nav>
   );
 };
