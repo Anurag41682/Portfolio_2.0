f0f39b298d2aa4b45aec75919acd1748e99c7f12
Anurag41682 <anuragyadav41682@gmail.com>
2024-06-29 02:44:04 +0530

    Done Contact Section

--- src/section/contact/Contact.tsx
+++ src/section/contact/Contact.tsx
@@ -1,8 +1,24 @@
 import "./contact.css";
+import data from "./data";
 const Contact = () => {
   return (
     <section id="contact">
-      <h2>Contact</h2>
+      <h2>Get In Touch</h2>
+      <p>Shoot me a message via any of the link below</p>
+      <div className="container contact_container">
+        {data.map((item) => {
+          return (
+            <a
+              key={item.id}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          );
+        })}
+      </div>
     </section>
   );
 };
---
+++ src/section/contact/data.tsx
@@ -0,0 +1,14 @@
+import { IoMdMail } from "react-icons/io";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+const data = [
+  { id: 1, icon: <IoMdMail />, link: "mailto:anuragyadav41682@gmail.com" },
+  {
+    id: 2,
+    icon: <FaLinkedinIn />,
+    link: "https://www.linkedin.com/in/anurag41682/",
+  },
+  { id: 3, icon: <FaXTwitter />, link: "https://x.com/Anur4g_Yadav" },
+];
+
+export default data;
