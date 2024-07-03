ec56608b26189beb984a0192fb7a0a5771236e3e
Anurag41682 <anuragyadav41682@gmail.com>
2024-07-03 08:01:53 +0530

    Updated About

--- src/section/about/data.tsx
+++ src/section/about/data.tsx
@@ -1,7 +1,15 @@
 import { GiBlackBook } from "react-icons/gi";
-
+import { SiCodechef } from "react-icons/si";
+import { TbBrandLeetcode } from "react-icons/tb";
 const data = [
   { id: 1, icon: <GiBlackBook />, title: "Projects", desc: "5+ completed" },
+  { id: 3, icon: <TbBrandLeetcode />, title: "Leetcode", desc: "1780+ Rating" },
+  {
+    id: 2,
+    icon: <SiCodechef />,
+    title: "Codechef",
+    desc: "3 Star Rating",
+  },
 ];
 
 export default data;
